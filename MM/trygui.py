a0b56ef4dcb71903b920644c75de05b1542cc4eb
lianctrl <luca.sagresti@gmail.com>
2023-07-21 13:01:13 +0200

    tkinter as tk

--- MM/trygui.py
+++ MM/trygui.py
@@ -1,5 +1,6 @@
-from tkinter import *
+import tkinter as tk
 import tkinter.simpledialog as simpledialog
+
 
 class Circle:
     def __init__(self, canvas, x, y, name):
@@ -22,6 +23,7 @@ class Circle:
         for arrow in self.arrows:
             arrow.redraw()
 
+
 class Arrow:
     def __init__(self, canvas, start, end, name):
         self.canvas = canvas
@@ -37,22 +39,23 @@ class Arrow:
         self.canvas.coords(self.line, self.start.x, self.start.y, self.end.x, self.end.y)
         self.canvas.coords(self.label, (self.start.x+self.end.x)/2, (self.start.y+self.end.y)/2)
 
+
 class App:
     def __init__(self, master):
         self.master = master
-        self.canvas = Canvas(master, width=500, height=500)
+        self.canvas = tk.Canvas(master, width=500, height=500)
         self.canvas.pack()
         self.circles = []
         self.arrows = []
 
-        self.add_circle_button = Button(master, text='Add Circle', command=self.add_circle)
-        self.add_circle_button.pack(side=LEFT)
+        self.add_circle_button = tk.Button(master, text='Add Circle', command=self.add_circle)
+        self.add_circle_button.pack(side=tk.LEFT)
 
-        self.add_arrow_button = Button(master, text='Add Arrow', command=self.add_arrow)
-        self.add_arrow_button.pack(side=LEFT)
+        self.add_arrow_button = tk.Button(master, text='Add Arrow', command=self.add_arrow)
+        self.add_arrow_button.pack(side=tk.LEFT)
 
-        self.clear_button = Button(master, text='Clear All', command=self.clear)
-        self.clear_button.pack(side=LEFT)
+        self.clear_button = tk.Button(master, text='Clear All', command=self.clear)
+        self.clear_button.pack(side=tk.LEFT)
 
     def add_circle(self):
         name = simpledialog.askstring('Circle Name', 'Enter a name for the circle:')
@@ -78,7 +81,6 @@ class App:
         arrow = Arrow(self.canvas, start, end, name)
         self.arrows.append(arrow)
 
-
     def clear(self):
         for circle in self.circles:
             self.canvas.delete(circle.item)
@@ -90,8 +92,6 @@ class App:
         self.arrows = []
 
 
-
-root = Tk()
+root = tk.Tk()
 app = App(root)
 root.mainloop()
-
