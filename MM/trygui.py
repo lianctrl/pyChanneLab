67f56f14b94fbe270ab493835b39b4fd5d7c5bf4
lianctrl <luca.sagresti@gmail.com>
2023-07-21 13:06:16 +0200

    tkinter as tk

--- MM/trygui.py
+++ MM/trygui.py
@@ -29,15 +29,19 @@ class Arrow:
         self.canvas = canvas
         self.start = start
         self.end = end
-        self.line = canvas.create_line(start.x, start.y, end.x, end.y, arrow='last')
+        self.line = canvas.create_line(start.x, start.y, end.x, end.y,
+                                       arrow='last')
         start.arrows.append(self)
         end.arrows.append(self)
         self.name = name
-        self.label = canvas.create_text((start.x+end.x)/2, (start.y+end.y)/2, text=name)
+        self.label = canvas.create_text((start.x+end.x)/2, (start.y+end.y)/2,
+                                        text=name)
 
     def redraw(self):
-        self.canvas.coords(self.line, self.start.x, self.start.y, self.end.x, self.end.y)
-        self.canvas.coords(self.label, (self.start.x+self.end.x)/2, (self.start.y+self.end.y)/2)
+        self.canvas.coords(self.line, self.start.x, self.start.y, self.end.x,
+                           self.end.y)
+        self.canvas.coords(self.label, (self.start.x+self.end.x)/2,
+                           (self.start.y+self.end.y)/2)
 
 
 class App:
@@ -48,13 +52,16 @@ class App:
         self.circles = []
         self.arrows = []
 
-        self.add_circle_button = tk.Button(master, text='Add Circle', command=self.add_circle)
+        self.add_circle_button = tk.Button(master, text='Add Circle',
+                                           command=self.add_circle)
         self.add_circle_button.pack(side=tk.LEFT)
 
-        self.add_arrow_button = tk.Button(master, text='Add Arrow', command=self.add_arrow)
+        self.add_arrow_button = tk.Button(master, text='Add Arrow',
+                                          command=self.add_arrow)
         self.add_arrow_button.pack(side=tk.LEFT)
 
-        self.clear_button = tk.Button(master, text='Clear All', command=self.clear)
+        self.clear_button = tk.Button(master, text='Clear All',
+                                      command=self.clear)
         self.clear_button.pack(side=tk.LEFT)
 
     def add_circle(self):
@@ -67,13 +74,15 @@ class App:
     def add_arrow(self):
         if len(self.circles) < 2:
             return
-        start_index = simpledialog.askinteger('Start Circle', 'Enter the index of the start circle:')
+        start_index = simpledialog.askinteger('Start Circle', 'Enter the index'
+                                              'of the start circle:')
         if start_index is None or start_index < 0 or start_index >= len(self.circles):
             return
-        end_index = simpledialog.askinteger('End Circle', 'Enter the index of the end circle:')
+        end_index = simpledialog.askinteger('End Circle', 'Enter the index of'
+                                            ' the end circle:')
         if end_index is None or end_index < 0 or end_index >= len(self.circles) or end_index == start_index:
             return
-        name = simpledialog.askstring('Arrow Name', 'Enter a name for the arrow:')
+        name = simpledialog.askstring('Arrow Name', 'Enter name for the arrow:')
         if not name:
             return
         start = self.circles[start_index]
