d1ce60b4a5f251238301bd314252b1b116b91740
lianctrl <luca.sagresti@gmail.com>
2023-07-21 16:11:10 +0200

    gui with arrows

--- MM/MMgui.py
+++ MM/MMgui.py
@@ -1,162 +1,144 @@
 import tkinter as tk
+import tkinter.simpledialog as simpledialog
 
-#class MarkovModelEditor(tk.Frame):
-#    def __init__(self, master=None):
-#        super().__init__(master)
-#        self.master = master
-#        self.master.title("Markov Model Editor")
-#        self.pack()
-#        
-#        self.states = []
-#        self.transition_probabilities = {}
-#        
-#        self.canvas = tk.Canvas(self, width=500, height=500, bg='white')
-#        self.canvas.pack()
-#        
-#        self.current_state = None
-#        self.canvas.bind('<Button-1>', self.create_state)
-#        
-#        self.selected_state = None
-#        self.canvas.bind('<Button-2>', self.select_state)
-#        self.canvas.bind('<Button-3>', self.create_transition)
-#        
-#    def create_state(self, event):
-#        x, y = event.x, event.y
-#        state_name = f'State{len(self.states)}'
-#        self.canvas.create_oval(x-25, y-25, x+25, y+25, fill='lightblue', tags=state_name)
-#        self.canvas.create_text(x, y, text=state_name, tags=state_name)
-#        self.add_state(state_name)
-#        
-#    def add_state(self, state):
-#        self.states.append(state)
-#        self.transition_probabilities[state] = {}
-#        
-#    def select_state(self, event):
-#        item = self.canvas.find_closest(event.x, event.y)[0]
-#        tags = self.canvas.itemcget(item, 'tags')
-#        if 'State' in tags:
-#            if self.selected_state is not None:
-#                self.canvas.itemconfig(self.selected_state, outline='black')
-#            self.selected_state = item
-#            self.canvas.itemconfig(item, outline='red')
-#        
-#    def create_transition(self, event):
-#        if self.selected_state is not None:
-#            x, y = event.x, event.y
-#            from_state = self.canvas.itemcget(self.selected_state, 'tags').split()[0]
-#            to_state = None
-#            for item in self.canvas.find_withtag('State'):
-#                if item != self.selected_state:
-#                    bbox = self.canvas.bbox(item)
-#                    if bbox[0] <= x <= bbox[2] and bbox[1] <= y <= bbox[3]:
-#                        to_state = self.canvas.itemcget(item, 'tags').split()[0]
-#                        break
-#            if to_state is not None:
-#                self.canvas.create_line(event.x, event.y, self.canvas.coords(self.selected_state)[0]+25, self.canvas.coords(self.selected_state)[1]+25, arrow='last', tags=(from_state, to_state))
-#                self.add_transition(from_state, to_state, 0.5)
-#        
-#    def add_transition(self, from_state, to_state, probability):
-#        self.transition_probabilities[from_state][to_state] = probability
-#        
-#    def get_transition_probabilities(self):
-#        return self.transition_probabilities
-#    
-#    def clear(self):
-#        self.canvas.delete('all')
-#        self.states = []
-#        self.transition_probabilities = {}
-#        self.selected_state = None
 
-import tkinter as tk
+class State:
+    def __init__(self, canvas, x, y, name):
+        self.canvas = canvas
+        self.item = canvas.create_oval(x-20, y-20, x+20, y+20,
+                                       fill='lightblue', outline='black')
+        self.canvas.tag_bind(self.item, '<Button1-Motion>', self.move)
+        self.x = x
+        self.y = y
+        self.arrows = []
+        self.name = name
+        self.label = canvas.create_text(x, y+30, text=name)
 
-class MarkovModelEditor(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
+    def move(self, event):
+        dx = event.x - self.x
+        dy = event.y - self.y
+        self.canvas.move(self.item, dx, dy)
+        self.canvas.move(self.label, dx, dy)
+        self.x = event.x
+        self.y = event.y
+        for arrow in self.arrows:
+            arrow.redraw()
+
+
+class Transition:
+    def __init__(self, canvas, start, end, name, two_way=False):
+        self.canvas = canvas
+        self.start = start
+        self.end = end
+        self.two_way = two_way
+        self.line = canvas.create_line(start.x, start.y, end.x, end.y,
+                                       arrow='last')
+        start.arrows.append(self)
+        end.arrows.append(self)
+        self.name = name
+        self.label_up = canvas.create_text(0, 0, text=name)
+        self.label_down = canvas.create_text(0, 0, text=name)
+        self.redraw()
+
+    def redraw(self):
+        self.canvas.coords(self.line, self.start.x, self.start.y, self.end.x, self.end.y)
+        mid_x = (self.start.x + self.end.x) / 2
+        mid_y = (self.start.y + self.end.y) / 2
+
+        angle = tk.Canvas(self.canvas).create_line(self.start.x, self.start.y, self.end.x, self.end.y)
+        angle = tk.Canvas(self.canvas).itemcget(angle, 'angle')
+        angle = float(angle)
+
+        label_distance = 15
+
+        angle_offset = 30
+        self.canvas.coords(self.label_up, mid_x + label_distance * math.cos(math.radians(angle + angle_offset)),
+                           mid_y + label_distance * math.sin(math.radians(angle + angle_offset)))
+        self.canvas.coords(self.label_down, mid_x + label_distance * math.cos(math.radians(angle - angle_offset)),
+                           mid_y + label_distance * math.sin(math.radians(angle - angle_offset)))
+
+    def delete(self):
+        self.canvas.delete(self.line)
+        self.canvas.delete(self.label_up)
+        self.canvas.delete(self.label_down)
+
+
+class App:
+    def __init__(self, master):
         self.master = master
         self.master.title("Markov Model Editor")
-        self.pack()
-        
-        self.states = []
-        self.transition_probabilities = {}
-        
-        self.canvas = tk.Canvas(self, width=500, height=500, bg='white')
+        self.canvas = tk.Canvas(master, width=500, height=500)
         self.canvas.pack()
-        
-        # Create buttons for adding states and transitions
-        self.add_state_button = tk.Button(self, text='Add State', command=self.create_state)
-        self.add_state_button.pack(side='left')
-        self.add_transition_button = tk.Button(self, text='Add Transition', command=self.create_transition)
-        self.add_transition_button.pack(side='left')
-        
-        self.current_state = None
-        self.canvas.bind('<Button-1>', self.select_state)
-        
-        self.selected_state = None
-        self.selected_transition = None
-        
-    def create_state(self):
-        state_name = f'State{len(self.states)}'
-        self.canvas.create_oval(50, 50, 150, 150, fill='lightblue', tags=state_name)
-        self.canvas.create_text(100, 100, text=state_name, tags=state_name)
-        self.add_state(state_name)
-        
-    def add_state(self, state):
-        self.states.append(state)
-        self.transition_probabilities[state] = {}
-        
-    def select_state(self, event):
-        item = self.canvas.find_closest(event.x, event.y)[0]
-        tags = self.canvas.itemcget(item, 'tags')
-        if 'State' in tags:
-            if self.selected_state is not None:
-                self.canvas.itemconfig(self.selected_state, outline='black')
-            self.selected_state = item
-            self.selected_transition = None
-            self.canvas.itemconfig(item, outline='red')
-        
-    def create_transition(self):
-        if self.selected_state is not None:
-            self.selected_transition = None
-            self.canvas.bind('<Button-1>', self.select_transition_start)
-            self.canvas.bind('<ButtonRelease-1>', self.select_transition_end)
-            self.canvas.bind('<Motion>', self.draw_transition)
-        
-    def select_transition_start(self, event):
-        item = self.canvas.find_closest(event.x, event.y)[0]
-        tags = self.canvas.itemcget(item, 'tags')
-        if 'State' in tags:
-            self.selected_transition = {'from_state': tags.split()[0], 'start_coords': (event.x, event.y)}
-        
-    def select_transition_end(self, event):
-        if self.selected_transition is not None:
-            item = self.canvas.find_closest(event.x, event.y)[0]
-            tags = self.canvas.itemcget(item, 'tags')
-            if 'State' in tags:
-                self.add_transition(self.selected_transition['from_state'], tags.split()[0], 0.5)
-                self.selected_transition = None
-                self.canvas.unbind('<Button-1>')
-                self.canvas.unbind('<ButtonRelease-1>')
-                self.canvas.unbind('<Motion>')
-        
-    def draw_transition(self, event):
-        if self.selected_transition is not None:
-            coords = self.selected_transition['start_coords'] + (event.x, event.y)
-            if self.selected_transition.get('line') is None:
-                self.selected_transition['line'] = self.canvas.create_line(*coords, arrow='last')
-            else:
-                self.canvas.coords(self.selected_transition['line'], *coords)
-        
-    def add_transition(self, from_state, to_state, probability):
-        self.transition_probabilities[from_state][to_state] = probability
+        self.circles = {}
+        self.arrows = []
 
-    def get_transition_probabilities(self):
-        return self.transition_probabilities
+        self.add_circle_button = tk.Button(master, text='Add State',
+                                           command=self.add_circle)
+        self.add_circle_button.pack(side=tk.LEFT)
+
+        self.add_arrow_button = tk.Button(master, text='Add Transition',
+                                          command=self.add_arrow)
+        self.add_arrow_button.pack(side=tk.LEFT)
+
+        self.clear_button = tk.Button(master, text='Clear All',
+                                      command=self.clear)
+        self.clear_button.pack(side=tk.LEFT)
+
+    def add_circle(self):
+        name = simpledialog.askstring('State Name', 'Enter a name for the State:')
+        if not name:
+            return
+        x, y = 250, 250
+        states = State(self.canvas, x, y, name)
+        self.circles[name] = states
+
+    def add_arrow(self):
+        if len(self.circles) < 2:
+            return
+        start_name = simpledialog.askstring('Start State', 'Enter the name of the starting state:')
+        if not start_name or start_name not in self.circles:
+            return
+        end_name = simpledialog.askstring('End State', 'Enter the name of the ending state:')
+        if not end_name or end_name not in self.circles or end_name == start_name:
+            return
+        name = simpledialog.askstring('Transition Name', 'Enter name for the transition:')
+        if not name:
+            return
+
+        def two_way_yes():
+            self.create_arrow(start_name, end_name, name, two_way=True)
+
+        def two_way_no():
+            self.create_arrow(start_name, end_name, name, two_way=False)
+
+        two_way_frame = tk.Toplevel()
+        two_way_frame.title("Two-Way Transition")
+        two_way_label = tk.Label(two_way_frame, text="Create a two-way transition?")
+        two_way_label.pack()
+        two_way_yes_button = tk.Button(two_way_frame, text="Yes", command=two_way_yes)
+        two_way_yes_button.pack(side=tk.LEFT)
+        two_way_no_button = tk.Button(two_way_frame, text="No", command=two_way_no)
+        two_way_no_button.pack(side=tk.LEFT)
+
+    def create_arrow(self, start_name, end_name, name, two_way):
+        start = self.circles[start_name]
+        end = self.circles[end_name]
+        arrow = Transition(self.canvas, start, end, name, two_way=two_way)
+        self.arrows.append(arrow)
+        if two_way:
+            arrow2 = Transition(self.canvas, end, start, name, two_way=False)
+            self.arrows.append(arrow2)
+
+    def clear(self):
+        for circle in self.circles.values():
+            self.canvas.delete(circle.item)
+            self.canvas.delete(circle.label)
+        for arrow in self.arrows:
+            arrow.delete()
+        self.circles = {}
+        self.arrows = []
 
 
 root = tk.Tk()
-app = MarkovModelEditor(root)
+app = App(root)
 root.mainloop()
-
-
-
-
